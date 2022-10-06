e01ab5373c2663c5ce0e865469fcdc1da5d8bd7c
uKaaida <111130340+uKaaida@users.noreply.github.com>
2022-10-06 09:01:58 -0300

    Question 02 DONE

--- Atividade extra/Question02.java
+++ Atividade extra/Question02.java
@@ -4,26 +4,16 @@ public class Question02 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numero;
-        boolean NeverEnd = true;
         System.out.println("Digite um número maior que 0: ");
         numero = scan.nextInt();
-        while(NeverEnd){
-            while(numero < 0){
-                System.out.println("Número inválido!!!!!\nDigite um número maior que 0: ");
-                numero = scan.nextInt();
-            }
-            while(numero%2==0){
+
+        while (numero >= 0) {
+            if (numero % 2 == 0) {
                 System.out.println("É PAR!!");
-                numero = scan.nextInt();
-            }
-            while(numero%3==0){
+            } else {
                 System.out.println("É IMPAR!!");
-                numero = scan.nextInt();
             }
+            numero = scan.nextInt();
         }
     }
 }
-
-
-
-//NÃO FINALIZADO!!!
